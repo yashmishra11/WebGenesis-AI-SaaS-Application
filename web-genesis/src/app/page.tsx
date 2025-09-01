09e9a121dd6f9636ae6ac8be3f7f260d4fcdbf3b
Mohammad Shan <mohdshan1024@gmail.com>
2025-09-01 11:44:19 +0530

    database_setup_01 is completed

--- web-genesis/src/app/page.tsx
+++ web-genesis/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 
-const Page = () => {
+const Page = async () => {
   return (
     <div className=" flex justify-center items-center flex-col h-[100vh] mt-5 font-bold">
       Hello, NEXTJS
-      <Button variant="destructive" className="mt-2">Click Me</Button>
+      <Button variant="destructive" className="mt-2">
+        Click Me
+      </Button>
     </div>
   );
 };
