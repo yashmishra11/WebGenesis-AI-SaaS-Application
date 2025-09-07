29a1eb4adef6aa0c1bf9a40aba1165e37aa59932
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2025-09-07 14:24:18 +0530

    now-i-can-do-my-part

--- web-genesis/src/app/page.tsx
+++ web-genesis/src/app/page.tsx
@@ -1,14 +1,18 @@
-import { getQueryClient, trpc} from "@/trpc/server";
+import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-// import ClientComponent from "./client";
+import { Suspense } from "react";
+import { Client } from "./client";
 
-  return (<HydrationBoundary state={dehydrate(queryClient)}>
-    <div>
-      
-    {/* <{JSON.stringify(dehydrate(queryClient))}>  
-     */}
-    </div>
+const page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text:"yash PREFETCH"}));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+     <Suspense fallback={<p>Loading...</p>}>
+      <Client />
+     </Suspense>
     </HydrationBoundary>
-  )
-}
- 
+  );
+};
+export default page;
