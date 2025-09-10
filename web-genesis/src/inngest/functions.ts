268fc16f7db34c9b03abac3df71eaaac79d73153
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2025-09-10 16:39:35 +0530

    05-ai-jobs

--- web-genesis/src/inngest/functions.ts
+++ web-genesis/src/inngest/functions.ts
@@ -1,15 +1,20 @@
+import { openai, createAgent } from "@inngest/agent-kit"; 
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("taking the input", "5s");
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer.  You summarize in 2 words.",
+      model: openai({ model: "gpt-4o", apiKey: process.env.OPENAI_API_KEY}),
+    });
 
-    await step.sleep("processing the input", "5s");
-
-    await step.sleep("finalizing", "5s");
-
-    return { message: `Hello ${event.data.email}!` };
+    const { output } = await summarizer.run(
+  `Summarize the following text: ${event.data.value}`,
+);
+    return { output };
   }
 );
