f3c3768f46c71a02ce8963a75a45ac794a89eb3e
Mohammad Shan <mohdshan1024@gmail.com>
2025-09-13 10:09:25 +0530

    feature/Toaster added

--- web-genesis/src/app/layout.tsx
+++ web-genesis/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gray-50`}
       >
         {/* ✅ TRPC Provider wrapped here so queries work in the entire app */}
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <Toaster />
+          {children}
+        </TRPCReactProvider>
       </body>
     </html>
   );
