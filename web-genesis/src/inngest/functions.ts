ecaa1546a7f4f57a57407832483a008138f703db
Mohammad Shan <mohdshan1024@gmail.com>
2025-09-16 20:36:39 +0530

    fixing function code to it's ororiginal form

--- web-genesis/src/inngest/functions.ts
+++ web-genesis/src/inngest/functions.ts
@@ -1,20 +1,22 @@
-import { openai, createAgent } from "@inngest/agent-kit"; 
-
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
+  { id: "webgenesis-ai-hello-world" },
   { event: "test/hello.world" },
   async ({ event }) => {
     const summarizer = createAgent({
       name: "summarizer",
-      system: "You are an expert summarizer.  You summarize in 2 words.",
-      model: openai({ model: "gpt-4o", apiKey: process.env.OPENAI_API_KEY}),
+      system: "You are an expert summarizer. You summarize in 2 words",
+      model: openai({ model: "gpt-4o" }),
     });
 
     const { output } = await summarizer.run(
-  `Summarize the following text: ${event.data.value}`,
-);
+      `Summarize the following text: ${event.data.value}`
+    );
+
+    console.log(output);
+
     return { output };
   }
 );
