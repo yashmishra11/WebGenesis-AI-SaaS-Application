51b311f4cd274a6dcca6394808c3e15c138bb27a
prajjval9579 <nikkibopche29420@gmail.com>
2025-11-07 22:28:04 +0530

    messages complete

--- web-genesis/src/app/api/inngest/route.ts
+++ web-genesis/src/app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { helloWorld } from "@/inngest/functions";
+import { codeAgentFunction } from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloWorld],
+  functions: [codeAgentFunction],
 });
