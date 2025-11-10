8351839cbfc801613a68e0b09462c5370ccd7f83
Mohammad Shan <mohdshan1024@gmail.com>
2025-11-10 14:31:12 +0530

    message ui completed

--- web-genesis/src/app/projects/[projectId]/page.tsx
+++ web-genesis/src/app/projects/[projectId]/page.tsx
@@ -3,6 +3,7 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
 
+// Interfaces
 interface Props {
   params: Promise<{
     projectId: string;
